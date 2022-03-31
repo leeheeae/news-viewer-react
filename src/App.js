bdc83cb5ac27b87eadcc38c784e9b097bf404d82
unknown <chromeee@daum.net>
2022-03-31 17:00:15 +0900

    NewsList, NewsItem, 임시데이터로 스타일링

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import { useState } from "react";
 import axios from "axios";
+import NewsList from "./components/NewsList";
 
 const App = () => {
-    const [data, setData] = useState(null);
-    const onClick = async () => {
-        try {
-            const response = await axios.get(
-                "https://newsapi.org/v2/top-headlines?country=kr&apikey=4f46e736be174af89f54440638fe7956"
-            );
-            setData(response.data);
-        } catch (err) {
-            console.error(err);
-        }
-    };
     return (
         <>
-            <div>
-                <button onClick={onClick}>불러오기</button>
-                {data && (
-                    <textarea
-                        rows={7}
-                        value={JSON.stringify(data, null, 2)}
-                        readOnly={true}
-                    />
-                )}
-            </div>
+            <NewsList />
         </>
     );
 };
