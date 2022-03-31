33760b3dba3828ebafc6f7f51a41e60f960ecbb7
unknown <chromeee@daum.net>
2022-03-31 23:45:14 +0900

    API를 이용하여 뉴스리스트 뿌리기

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,5 +1,7 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
+import axios from "axios";
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -21,12 +23,40 @@ const sampleArticle = {
 };
 
 const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    "https://newsapi.org/v2/top-headlines?country=kr&apikey=4f46e736be174af89f54440638fe7956"
+                );
+                setArticles(response.data.articles);
+            } catch (err) {
+                console.error(err);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    //대기중일때
+    if (loading) {
+        return <NewsListBlock>대기중...</NewsListBlock>;
+    }
+
+    //아직 articles 값이 설정되지 않았을 때
+    if (!articles) {
+        return null;
+    }
+
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
+            {articles.map((article) => (
+                <NewsItem key={article.url} article={article} />
+            ))}
         </NewsListBlock>
     );
 };
